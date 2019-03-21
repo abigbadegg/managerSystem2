4a26554fb33f9397ef3dc3d78dcd41581247a40d
南有乔木 <18890341135@qq.com>
2019-03-21 18:35:05 +0800

    注册功能添加1

--- src/main/webapp/login/welcome.js
+++ src/main/webapp/login/welcome.js
@@ -4,9 +4,38 @@ function mylogin(){
     user.role = $("input[name='role']").val();
     user.name = $("input[name='name']").val();
     user.password = $("input[name='password']").val();
-    user.id = 1;
 
     //信息校验
+    checkInfo(user);
+    //检查是否存在该用户
+    var isUser = getUser(user);
+
+    if(isUser > 0){
+        alert("登录成功！！！");
+    }else{
+        alert("账号或密码错误！！！");
+    }
+}
+
+function myRegister(){
+    var user = new Object();
+    user.role = $("input[name='role']").val();
+    user.name = $("input[name='name']").val();
+    user.password = $("input[name='password']").val();
+
+    //信息校验
+    checkInfo(user);
+    //检查是否存在该用户
+    var isUser = getUser(user);
+
+    if(isUser > 0){
+        alert("该用户名已存在");
+        return;
+    }
+    var num = setUser(user);
+}
+
+function checkInfo(user){
     if(user.role != 1 && user.role != 2){
         alert("请选择登录身份");
         return;
@@ -16,25 +45,22 @@ function mylogin(){
         return;
     }
     if(user.password == null || user.password.length < 3 || user.password.length > 16){
-        alert("请规范填写密码，长度在6到12位之间");
+        alert("请规范填写密码，长度在3到12位之间");
         return;
     }
-
-    //检查是否存在该用户
-    var isUser = getUser(user);
 }
 
 //检查是否存在该用户 com.manager.controller.
 function getUser(user){
     $.post("./userController/getUser.do",user,function(result,state){
-        if(result > 0){
-            alert("登录成功！！！");
-        }else{
-            alert("账号或密码错误！！！");
-        }
+        var isUser = result;
     });
+    return isUser;
 }
 
-function myRegister(){
-
+function setUser(user){
+    $.post("./userController/setUser.do",user,function(result,state){
+        var num = result;
+    });
+    return num;
 }
